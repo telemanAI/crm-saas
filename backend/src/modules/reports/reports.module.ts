30a1f8d3688b6f00d30454844880b56a1fa00540
telemanAI <raimondo462@gmail.com>
2026-03-25 16:15:35 +0100

    backend

--- backend/src/modules/reports/reports.module.ts
+++ backend/src/modules/reports/reports.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { WashReportController } from './wash-report.controller';
 import { Practice } from '../practices/entities/practice.entity';
+import { Tenant } from '../tenants/entities/tenant.entity'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Practice])],

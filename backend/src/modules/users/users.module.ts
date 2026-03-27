4fb0f8f9f871c7350a196a1aef3f526969922b7d
telemanAI <raimondo462@gmail.com>
2026-03-27 12:38:56 +0100

    correzione errore conferma email operatore// crash backend

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { TenantsModule } from '../tenants/tenants.module'; // ✅ Aggiungi questo import
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    TenantsModule, // ✅ Aggiungi qui
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
   exports: [UsersService],
 })
 export class UsersModule {}
-

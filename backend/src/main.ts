e45f732a8cdca013928b14b9bc3f56b358146d74
telemanAI <raimondo462@gmail.com>
2026-05-05 12:21:57 +0200

    fix main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   // Helmet - Sicurezza headers
   app.use(helmet({
